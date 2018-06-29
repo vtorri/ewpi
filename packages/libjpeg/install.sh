ee7792ec98582048aee71de43267d5d0d28baea7
Vincent Torri <vincent.torri@gmail.com>
2018-06-29 23:16:04 +0200

    Fix compilation of libjpeg on linux

--- packages/libjpeg/install.sh
+++ packages/libjpeg/install.sh
@@ -9,25 +9,5 @@
 cd packages/$1 > /dev/null
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
-if test "x$host" = "xi686-w64-mingw32" ; then
-    arch=X86
-else
-    arch=AMD64
-fi
-ar_exe=`where ar.exe`
-rm -rf build && \
-mkdir build && \
-cd build && \
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$3 \
-    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
-    -DCMAKE_SYSTEM_NAME=Windows \
-    -DCMAKE_C_COMPILER=$4-gcc \
-    -DCMAKE_RC_COMPILER=$4-windres \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_AR=$ar_exe \
-    -DENABLE_STATIC:BOOL=OFF \
-    -DWITH_TURBOJPEG:BOOL=OFF \
-    -G "MSYS Makefiles" \
-    .. > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --enable-shared  > ../config.log 2>&1
 make -j install > ../make.log 2>&1
