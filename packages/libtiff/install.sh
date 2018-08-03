3c477619e8595181ee035b7d4b6d8889203f889a
Vincent Torri <vincent.torri@gmail.com>
2018-08-03 06:55:21 +0200

    libtiff: add pkg-config dependency

--- packages/libtiff/install.sh
+++ packages/libtiff/install.sh
@@ -9,6 +9,11 @@
 cd packages/$1 > /dev/null
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+export CPPFLAGS=-I$3/include
+export LDFLAGS=-L$3/lib
 autoreconf -vif  > ../config.log 2>&1 &&
 ./configure --prefix=$3 --host=$4 --disable-static --with-zlib-include-dir=$3/include --with-zlib-lib-dir=$3/lib --with-jpeg-include-dir=$3/include --with-jpeg-lib-dir=$3/lib --with-lzma-include-dir=$3/include --with-lzma-lib-dir=$3/lib >> ../config.log 2>&1
 make V=0 -j install > ../make.log 2>&1
