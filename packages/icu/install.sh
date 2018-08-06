aa42d199ecdffd13dbde7511b8276dd409ebc653
Vincent Torri <vincent.torri@gmail.com>
2018-08-06 14:07:13 +0200

    add icu package

---
+++ packages/icu/install.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# $1 : name
+# $2 : tarname
+# $3 : prefix
+# $4 : host
+# $5 : taropt2
+
+cd packages/$1 > /dev/null
+dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
+cd $dir_name/source
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+export CPPFLAGS=-I$3/include
+export LDFLAGS=-L$3/lib
+./configure --prefix=$3 --host=$4 --disable-static --disable-tests --disable-samples
+# > ../config.log 2>&1
+make -j
+# > ../make.log 2>&1
+make -j install
+# >> ../make.log 2>&1
