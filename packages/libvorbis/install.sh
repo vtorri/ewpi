9323534362f24b3001f05bca0b01fe70f75de3e8
Vincent Torri <vincent.torri@gmail.com>
2018-08-06 13:37:12 +0200

    libvorbis : add pkg-config support

--- packages/libvorbis/install.sh
+++ packages/libvorbis/install.sh
@@ -9,7 +9,10 @@
 cd packages/$1 > /dev/null
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
+export PKG_CONFIG_DIR=
 export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
-export PKG_CONFIG_PATH=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+export CPPFLAGS=-I$3/include
+export LDFLAGS=-L$3/lib
 ./configure --prefix=$3 --host=$4 --disable-static --disable-oggtest > ../config.log 2>&1
 make -j install > ../make.log 2>&1
