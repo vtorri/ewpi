c4a1dceaed136bd8d1eaaa453a74fbb6c9730277
Vincent Torri <vincent.torri@gmail.com>
2018-08-07 06:43:28 +0200

    fontconfig: add gperf dependency

--- packages/fontconfig/install.sh
+++ packages/fontconfig/install.sh
@@ -15,5 +15,5 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
 export CPPFLAGS=-I$3/include
 export LDFLAGS=-L$3/lib
-./configure --prefix=$3 --host=$4 --disable-static --enable-iconv --with-libiconv=$3 --with-libiconv-includes=$3/include --with-libiconv-lib=$3/lib --with-expat-includes=$3/include --with-expat-lib=$3/lib --disable-docs > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --enable-iconv --with-libiconv=$3 --with-expat=$3 --disable-docs > ../config.log 2>&1
 make -j install > ../make.log 2>&1
