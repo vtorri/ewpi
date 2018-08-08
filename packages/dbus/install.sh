9480a2c65e9eb9ed1e3433111941f2d3fd83e40c
Vincent Torri <vincent.torri@gmail.com>
2018-08-08 06:32:33 +0200

    dbus : use sed instead of patch, add pkg-config and expat as deps

--- packages/dbus/install.sh
+++ packages/dbus/install.sh
@@ -9,6 +9,11 @@
 cd packages/$1 > /dev/null
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
-autoreconf -vif ../config.log 2>&1 &&
-./configure --prefix=$3 --host=$4 --disable-static --disable-embedded-tests --disable-modular-tests --disable-tests >> ../config.log 2>&1
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+export CPPFLAGS=-I$3/include
+export LDFLAGS=-L$3/lib
+# autoreconf -vif ../config.log 2>&1 &&
+./configure --prefix=$3 --host=$4 --disable-static --disable-embedded-tests --disable-modular-tests --disable-tests > ../config.log 2>&1
 make -j install > ../make.log 2>&1
--- packages/dbus/pre.sh
+++ packages/dbus/pre.sh
@@ -7,4 +7,5 @@
 
 cd packages/$1
 tar $2 $3 > pre.log 2>&1
-patch -p0 < Makefile.diff >> pre.log 2>&1
+dir_name=`tar $4 $3 | head -1 | cut -f1 -d"/"`
+sed -i -e 's/SUBDIRS = dbus bus tools test doc/SUBDIRS = dbus bus tools test doc/g' $dir_name/Makefile.in
