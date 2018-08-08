1e42a1746ffa7c01029284f205de8feace532341
Vincent Torri <vincent.torri@gmail.com>
2018-08-08 05:43:52 +0200

    harfbuzz : less verbosity

--- packages/harfbuzz/install.sh
+++ packages/harfbuzz/install.sh
@@ -14,7 +14,5 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
 export CPPFLAGS=-I$3/include
 export LDFLAGS=-L$3/lib
-./configure --prefix=$3 --host=$4 --disable-static --with-glib=no --disable-gtk-doc-html --disable-gtk-doc-pdf --with-cairo=no --with-fontconfig=no
-# > ../config.log 2>&1
-make install
-# > ../make.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --with-glib=no --disable-gtk-doc-html --disable-gtk-doc-pdf --with-cairo=no --with-fontconfig=no > ../config.log 2>&1
+make install > ../make.log 2>&1
