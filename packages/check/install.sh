6a701e97360939628a748ce5adb87d3cf4f22864
Vincent Torri <vincent.torri@gmail.com>
2018-08-30 12:18:01 +0200

    check : disable subunit for now

--- packages/check/install.sh
+++ packages/check/install.sh
@@ -9,5 +9,5 @@
 cd packages/$1 > /dev/null
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
-./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --disable-subunit > ../config.log 2>&1
 make -j install > ../make.log 2>&1
