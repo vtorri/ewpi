78212beec69acddc600de3da0fe32fd4827d11f2
Vincent Torri <vincent.torri@gmail.com>
2018-08-30 11:00:53 +0200

    luajit: try to fix installation

--- packages/luajit/install.sh
+++ packages/luajit/install.sh
@@ -9,8 +9,31 @@
 cd packages/$1
 dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
-arch=
+arch="-m64"
 if test "x$4" = "xi686-w64-mingw32"; then
     arch="-m32"
 fi
-make -j install PREFIX=$3 HOST_CC="gcc $arch" CROSS=$4- TARGET_SYS=Windows > ../make.log 2>&1
+sed -i -e 's%$(INSTALL_X) $(FILE_T) $(INSTALL_T)%cp $(FILE_T) $(INSTALL_BIN)/luajit.tmp%g' Makefile
+sed -i -e 's%INSTALL_DYN= $(INSTALL_LIB)/$(INSTALL_SONAME)%INSTALL_DYN= $(INSTALL_BIN)/$(INSTALL_SONAME)%g' Makefile
+sed -i -e 's%$(SYMLINK) $(INSTALL_SONAME) $(INSTALL_SHORT1) &&%$(INSTALL_F) $(INSTALL_SOSHORT1) $(INSTALL_SHORT1) ||%g' Makefile
+sed -i -e 's%$(SYMLINK) $(INSTALL_SONAME) $(INSTALL_SHORT2) || :% :%g' Makefile
+sed -i -e 's%$(SYMLINK) $(INSTALL_TNAME) $(INSTALL_TSYM)%%g' Makefile
+make \
+    install \
+    PREFIX=$3 \
+    HOST_CC="gcc $arch" \
+    CROSS=$4- \
+    TARGET_SYS=Windows \
+    BUILDMODE=dynamic \
+    SYMLINK=cp \
+    INSTALL_X=cp \
+    INSTALL_F=cp \
+    FILE_T=luajit.exe \
+    INSTALL_TNAME=luajit.exe \
+    INSTALL_TSYMNAME=luajit.exe \
+    FILE_SO=lua51.dll \
+    INSTALL_SONAME=lua51.dll \
+    TARGET_XSHLDFLAGS="-shared -Wl,--out-implib,liblua.dll.a" \
+    INSTALL_SOSHORT1=liblua.dll.a \
+    > ../make.log 2>&1
+mv $3/bin/luajit.tmp $3/bin/luajit.exe
