f86107e4d7f5ec4c7609af472d83a1c02273d86d
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 06:33:24 +0200

    iconv: set to installed if successfull

--- packages/iconv/install.sh
+++ packages/iconv/install.sh
@@ -11,3 +11,4 @@ dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
 make clean > ../make.log 2>&1
 make -j install prefix=$3 CC=$4-gcc AR=$4-ar RANLIB=$4-ranlib DLLTOOL=$4-dlltool >> ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
