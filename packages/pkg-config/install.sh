72910d5678944604c3898372b3b5d9446df4e214
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 14:07:39 +0200

    pkg-config: set to installed if successfull

--- packages/pkg-config/install.sh
+++ packages/pkg-config/install.sh
@@ -11,3 +11,4 @@ dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
 ./configure --prefix=$3 --host=$4 --disable-static --with-internal-glib=yes &> ../config.log
 make -j install &> ../make.log
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
