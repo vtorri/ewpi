29d12e693eabef49e3aeade0b94dff32f87608da
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 06:37:56 +0200

    libpsl: set to installed if successfull

--- packages/libpsl/install.sh
+++ packages/libpsl/install.sh
@@ -16,3 +16,4 @@ export CPPFLAGS=-I$3/include
 export LDFLAGS="-L$3/lib -liconv -lws2_32"
 ./configure --prefix=$3 --host=$4 --disable-static --disable-gtk-doc-html --disable-man --with-libiconv-prefix=$3 > ../config.log 2>&1
 make -j install > ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
