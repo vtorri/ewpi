3a9cc1d3aa8127db92fbce5ba148b1853143fd29
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 14:08:11 +0200

    xz: set to installed if successfull

--- packages/xz/install.sh
+++ packages/xz/install.sh
@@ -11,3 +11,4 @@ dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
 ./configure --prefix=$3 --host=$4 --disable-static --enable-threads=vista --disable-lzmainfo --disable-lzma-links --disable-scripts --disable-doc --disable-cxx > ../config.log 2>&1
 make V=0 -j install > ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
