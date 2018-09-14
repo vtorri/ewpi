bac76a66a097278ae7cf9353528fcb70b1f064eb
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 05:55:50 +0200

    c-ares: set to installed if successfull

--- packages/cares/install.sh
+++ packages/cares/install.sh
@@ -11,3 +11,4 @@ dir_name=`tar $5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
 CPPFLAGS="-D_WIN32_WINNT=0x0600" ./configure --prefix=$3 --host=$4 --disable-static  > ../config.log 2>&1
 make V=0 -j install > ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
