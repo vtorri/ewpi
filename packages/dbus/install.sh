325a2009857f7f99f058a64735ec2352a4c5a686
Vincent Torri <vincent.torri@gmail.com>
2018-09-14 05:57:04 +0200

    dbus: set to installed if successfull

--- packages/dbus/install.sh
+++ packages/dbus/install.sh
@@ -14,6 +14,6 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
 export CPPFLAGS=-I$3/include
 export LDFLAGS=-L$3/lib
-# autoreconf -vif ../config.log 2>&1 &&
 ./configure --prefix=$3 --host=$4 --disable-static --disable-embedded-tests --disable-modular-tests --disable-tests > ../config.log 2>&1
 make -j install > ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
