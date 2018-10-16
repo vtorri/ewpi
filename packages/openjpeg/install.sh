22964e98eafe0510815db66ef0fc84a920347e51
Vincent Torri <vincent.torri@gmail.com>
2018-10-16 14:59:23 +0200

    openjpeg : don't run cmake in build/ subdir

--- packages/openjpeg/install.sh
+++ packages/openjpeg/install.sh
@@ -13,26 +13,21 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_PATH=$3/lib/pkgconfig
 EWPI_OS=`uname`
 case ${EWPI_OS} in
-    MSYS*)
-        EWPI_makefile="MSYS Makefiles"
-    ;;
-    MINGW*)
-        EWPI_makefile="MSYS Makefiles"
+    MSYS*|MINGW*)
+	prefix_unix=`cygpath -u $3`
     ;;
     *)
-        EWPI_makefile="Unix Makefiles"
+	prefix_unix=$3
     ;;
 esac
-rm -rf build && \
-mkdir build && \
-cd build && \
 cmake \
-    -DCMAKE_INSTALL_PREFIX=$3 \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
     -DCMAKE_VERBOSE_MAKEFILE=TRUE \
     -DCMAKE_C_COMPILER=$4-gcc \
     -DCMAKE_CXX_COMPILER=$4-g++ \
     -DCMAKE_RC_COMPILER=$4-windres \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_STATIC_LIBS:BOOL=OFF \
     -DBUILD_CODEC:BOOL=OFF \
     -DBUILD_JPWL:BOOL=OFF \
     -DBUILD_MJ2:BOOL=OFF \
@@ -40,9 +35,9 @@ cmake \
     -DBUILD_JP3D:BOOL=OFF \
     -DBUILD_PKGCONFIG_FILES:BOOL=ON \
     -DCMAKE_SYSTEM_NAME=Windows \
-    -G "$EWPI_makefile" \
-    .. > ../config.log 2>&1
+    -G "Unix Makefiles" \
+    . > ../config.log 2>&1
 
 make -j install > ../make.log 2>&1
 
-sed -i -e 's/installed: no/installed: yes/g' ../../$1.ewpi
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
