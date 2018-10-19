8e83475f1a0c71c4dd17abef885c7bcf71d1a33b
Vincent Torri <vincent.torri@gmail.com>
2018-10-19 20:37:29 +0200

    dbus : do not compile tests

--- packages/dbus/pre.sh
+++ packages/dbus/pre.sh
@@ -8,4 +8,4 @@
 cd packages/$1
 tar $2 $3 > pre.log 2>&1
 dir_name=`tar $4 $3 | head -1 | cut -f1 -d"/"`
-sed -i -e 's/SUBDIRS = dbus bus tools test doc/SUBDIRS = dbus bus tools test doc/g' $dir_name/Makefile.in
+sed -i -e 's/SUBDIRS = dbus bus tools test doc/SUBDIRS = dbus bus tools doc/g' $dir_name/Makefile.in
