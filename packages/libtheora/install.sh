a5e4d52c94f5e6bdd31de1ac31e637b574437d71
Vincent Torri <vincent.torri@gmail.com>
2018-10-30 20:55:17 +0100

    add libtheora

---
+++ packages/libtheora/install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# $1 : name
+# $2 : tarname
+# $3 : prefix
+# $4 : host
+# $5 : taropt
+
+cd packages/$1 > /dev/null
+dir_name=`tar t$5 $2 | head -1 | cut -f1 -d"/"`
+cd $dir_name
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+export CPPFLAGS=-I$3/include
+export LDFLAGS=-L$3/lib
+./configure --prefix=$3 --host=$4 --disable-static --disable-spec --disable-examples > ../config.log 2>&1
+make -j install > ../make.log 2>&1
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
---
+++ packages/libtheora/pre.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+# $1 : name
+# $2 : taropt
+# $3 : tarname
+
+cd packages/$1
+tar x$2 $3 > pre.log 2>&1
