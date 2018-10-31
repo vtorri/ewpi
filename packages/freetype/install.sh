98573b662d56e4495ace6069f720e98fb9c012a7
Vincent Torri <vincent.torri@gmail.com>
2018-10-31 09:48:55 +0100

    freetype: disable  bzip2 support

--- packages/freetype/install.sh
+++ packages/freetype/install.sh
@@ -14,6 +14,6 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
 export CPPFLAGS=-I$3/include
 export LDFLAGS=-L$3/lib
-./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --with-bzip2=no > ../config.log 2>&1
 make -j install > ../make.log 2>&1
 sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
