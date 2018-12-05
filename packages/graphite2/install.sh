7b9da9e98c56f472dac7f581138eff9ade5d095d
Vincent Torri <vincent.torri@gmail.com>
2018-12-05 21:04:08 +0100

    harfbuff: add graphite2 dependency

---
+++ packages/graphite2/install.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+set -e
+
+# $1 : name
+# $2 : tarname
+# $3 : prefix
+# $4 : host
+# $5 : taropt
+# $6 : jobopt
+
+cd packages/$1 > /dev/null
+dir_name=`tar t$5 $2 | head -1 | cut -f1 -d"/"`
+cd $dir_name
+sed -i -e 's/add_subdirectory(tests)//g' CMakeLists.txt
+EWPI_OS=`uname`
+case ${EWPI_OS} in
+    MSYS*|MINGW*)
+	prefix_unix=`cygpath -u $3`
+    ;;
+    *)
+	prefix_unix=$3
+    ;;
+esac
+cmake \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCMAKE_C_COMPILER=$4-gcc \
+    -DCMAKE_CXX_COMPILER=$4-g++ \
+    -DCMAKE_CXX_FLAGS="-I../src -I../include" \
+    -DCMAKE_SYSTEM_NAME=Windows \
+    -G "Unix Makefiles" \
+    . > ../config.log 2>&1
+
+make -j $jobopt install > ../make.log 2>&1
+
+sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
--- packages/harfbuzz/install.sh
+++ packages/harfbuzz/install.sh
@@ -17,6 +17,6 @@ export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
 export CPPFLAGS=-I$3/include
 export LDFLAGS=-L$3/lib
-./configure --prefix=$3 --host=$4 --disable-static --with-glib=no --disable-gtk-doc-html --disable-gtk-doc-pdf --with-cairo=no --with-fontconfig=no --with-freetype=yes --with-icu=no > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --with-glib=no --disable-gtk-doc-html --disable-gtk-doc-pdf --with-cairo=no --with-fontconfig=no --with-freetype=yes --with-graphite2=yes --with-icu=no > ../config.log 2>&1
 make -j $jobopt install > ../make.log 2>&1
 sed -i -e 's/installed: no/installed: yes/g' ../$1.ewpi
