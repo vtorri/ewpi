aff12c96cfed2073fd94d6493476d6d56cc797fb
Vincent Torri <vincent.torri@gmail.com>
2019-02-08 13:59:36 +0100

    libunistring: add iconv as dependency

--- packages/libunistring/install.sh
+++ packages/libunistring/install.sh
@@ -12,6 +12,6 @@ set -e
 dir_name=`tar t$5 $2 | head -1 | cut -f1 -d"/"`
 cd $dir_name
 
-./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --with-libiconv-prefix=$3 > ../config.log 2>&1
 
 make -j $jobopt install > ../make.log 2>&1
