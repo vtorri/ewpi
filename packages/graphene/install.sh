02a165df0fcd31b1621a34d27d4c3948fc64226f
Vincent Torri <vincent.torri@gmail.com>
2019-02-22 14:35:25 +0100

    add graphene (dependency of gst-plugins-base)

---
+++ packages/graphene/install.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+set -e
+
+# $1 : arch
+# $2 : tarname
+# $3 : prefix
+# $4 : host
+# $5 : taropt
+# $6 : jobopt
+
+dir_name=`tar t$5 $2 | head -1 | cut -f1 -d"/"`
+cd $dir_name
+cp ../cross_toolchain.txt .
+
+EWPI_OS=`uname`
+case ${EWPI_OS} in
+    MSYS*|MINGW*)
+	prefix_unix=`cygpath -u $3`
+    ;;
+    *)
+	prefix_unix=$3
+    ;;
+esac
+
+if test "x$4" = "xi686-w64-mingw32" ; then
+    sed -i -e 's/@cpu_family@/x86/g;s/@cpu@/i686/g' cross_toolchain.txt
+else
+    sed 's/@cpu_family@/x86_64/g;s/@cpu@/x86_64/g' cross_toolchain.txt
+fi
+
+sed -i -e "s/@host@/$4/g;s/@arch@/$1/g;s|@prefix@|$3|g" cross_toolchain.txt
+
+export PATH=$prefix_unix/bin:$PATH
+export PKG_CONFIG_DIR=
+export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
+export PKG_CONFIG_SYSROOOT_DIR=$3
+
+rm -rf builddir && mkdir builddir && cd builddir
+meson .. \
+      --prefix=$3 \
+      --libdir=lib \
+      --strip \
+      --cross-file ../cross_toolchain.txt \
+      --default-library shared \
+      -Dintrospection=false > ../../config.log 2>&1
+
+ninja -v install > ../../make.log 2>&1
