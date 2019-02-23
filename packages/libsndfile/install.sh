745845036a82df7b3b090d2bdbdf4c29380c3b99
Vincent Torri <vincent.torri@gmail.com>
2019-02-23 07:14:13 +0100

    update libsndfile to add speex support

--- packages/libsndfile/install.sh
+++ packages/libsndfile/install.sh
@@ -30,6 +30,6 @@ export CPPFLAGS=-I$3/include
 export CFLAGS="-O2 -pipe -march=$1 -mtune=$1"
 export LDFLAGS="-L$3/lib -s"
 
-./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --enable-experimental > ../config.log 2>&1
 
 make -j $jobopt install > ../make.log 2>&1
