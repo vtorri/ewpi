fd55084dd8e67542eafa60f17ccf183617719ba4
Vincent Torri <vincent.torri@gmail.com>
2019-03-06 10:21:04 +0100

    gettext: simplify install.sh script

--- packages/gettext/install.sh
+++ packages/gettext/install.sh
@@ -10,10 +10,7 @@ set -e
 # $6 : jobopt
 
 dir_name=`tar t$5 $2 | head -1 | cut -f1 -d"/"`
-cd $dir_name
-
-sed -i -e 's/gettext-runtime gettext-tools/gettext-runtime/g' configure
-sed -i -e 's/SUBDIRS = gnulib-local gettext-runtime gettext-tools/SUBDIRS = gnulib-local gettext-runtime/g' Makefile.in
+cd $dir_name/gettext-runtime
 
 EWPI_OS=`uname`
 case ${EWPI_OS} in
@@ -29,6 +26,6 @@ export CFLAGS="-O2 -pipe -march=$1 -mtune=$1"
 export CXXFLAGS="-O2 -pipe -march=$1 -mtune=$1"
 export LDFLAGS="-s"
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-c++ --disable-java--disable-native-java --enable-threads=windows --disable-rpath --disable-libasprintf --disable-curses --disable-acl --with-libiconv-prefix=$3 --with-libunistring-prefix=$3 > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --disable-c++ --disable-java --disable-native-java --enable-threads=windows --disable-rpath --disable-libasprintf --disable-curses --disable-acl --with-libiconv-prefix=$3 --with-libunistring-prefix=$3 > ../../config.log 2>&1
 
-make -j $jobopt install > ../make.log 2>&1
+make -j $jobopt install > ../../make.log 2>&1
