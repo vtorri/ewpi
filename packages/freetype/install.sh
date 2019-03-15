17005c7a8ec6d2df579ccbfb4422d4182c295008
Vincent Torri <vincent.torri@gmail.com>
2019-03-15 15:32:46 +0100

    freetype: update to 2.10.0

--- packages/freetype/install.sh
+++ packages/freetype/install.sh
@@ -26,9 +26,8 @@ export PATH=$prefix_unix/bin:$PATH
 export PKG_CONFIG_DIR=
 export PKG_CONFIG_LIBDIR=$3/lib/pkgconfig
 export PKG_CONFIG_SYSROOOT_DIR=$3
-export CPPFLAGS=-I$3/include
 export CFLAGS="-O2 -pipe -march=$1 -mtune=$1"
-export LDFLAGS="-L$3/lib -s"
+export LDFLAGS="-s"
 
 ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
