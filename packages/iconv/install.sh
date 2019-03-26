46f81bc4fb7a397f38567902130583cf0d54e3a2
Vincent Torri <vincent.torri@gmail.com>
2019-03-26 21:54:22 +0100

    iconv: add iconv.pc

--- packages/iconv/install.sh
+++ packages/iconv/install.sh
@@ -30,3 +30,20 @@ export LDFLAGS="-s"
 
 make clean > ../make.log 2>&1
 make -j $jobopt install prefix=$3 CC=$4-gcc AR=$4-ar RANLIB=$4-ranlib DLLTOOL=$4-dlltool >> ../make.log 2>&1
+
+cat > iconv.pc <<EOF
+prefix=$3
+exec_prefix=\${prefix}
+bindir=\${exec_prefix}/bin
+libdir=\${exec_prefix}/lib
+includedir=\${prefix}/include
+
+Name: iconv
+Description: iconv implementation using Win32 API to convert
+Version: 0.0.8
+Libs: -L\${libdir} -liconv
+Cflags: -DWINICONV_CONST -I\${includedir}
+EOF
+
+mkdir -p $3/lib/pkgconfig
+cp iconv.pc $3/lib/pkgconfig
