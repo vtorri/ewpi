05a1e7e7e00ae5717f18989df385e7821faad7a5
Vincent Torri <vincent.torri@gmail.com>
2019-04-29 14:16:44 +0200

    lz4: update to 1.9.1

--- packages/lz4/install.sh
+++ packages/lz4/install.sh
@@ -21,23 +21,4 @@ export CPPFLAGS=-I$3/include
 export CFLAGS="-O2 -pipe -march=$1"
 export LDFLAGS="-L$3/lib -s"
 
-sed -i \
-    -e 's/MidnightBSD/MidnightBSD MINGW32_NT-6.1 MINGW64_NT-6.1 MINGW32_NT-10.0.1 MINGW64_NT-10.0/g' \
-    -e 's|dll\\|dll/|g' \
-    lib/Makefile
-sed -i \
-    -e 's|$@.dll|$@.dll -Wl,--out-implib,liblz4.dll.a|g' \
-    -e 's|dlltool|#dlltool|g' \
-    lib/Makefile
-
-make -j $jobopt PREFIX=$3 BUILD_STATIC=no CC=$4-gcc SHARED_EXT_VER=1 V=1 OS=Windows_NT > ../make.log 2>&1
-
-mkdir -p $3/{bin,include,lib/pkgconfig}
-cp lz4.exe $3/bin
-cd lib
-cp dll/liblz4.1.dll $3/bin/liblz4-1.dll
-cp liblz4.dll.a $3/lib
-cp lz4.h lz4hc.h lz4frame.h $3/include
-make liblz4.pc PREFIX=$3 BUILD_STATIC=no CC=$4-gcc SHARED_EXT_VER=1 V=1 OS=Windows_NT >> ../../make.log 2>&1
-cp liblz4.pc $3/lib/pkgconfig
-cd ..
+make -j $jobopt PREFIX=$3 BUILD_STATIC=no CC=$4-gcc WINDRES=$4-windres SHARED_EXT_VER=1 V=1 OS=Windows_NT install > ../make.log 2>&1
