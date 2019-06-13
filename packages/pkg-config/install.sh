36b66f01e820755b44447f046886dfe6d4539cc1
Vincent Torri <vincent.torri@gmail.com>
2019-06-13 16:21:50 +0200

    pkg-config: fix stdout and stderr redirection

--- packages/pkg-config/install.sh
+++ packages/pkg-config/install.sh
@@ -2,6 +2,6 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --with-internal-glib=yes > ../config.log 2&>1
+./configure --prefix=$3 --host=$4 --disable-static --with-internal-glib=yes > ../config.log 2>&1
 
-make -j $5 $verbmake install > ../make.log 2&>1
+make -j $5 $verbmake install > ../make.log 2>&1
