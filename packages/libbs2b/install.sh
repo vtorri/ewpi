5dfa13c2a098c712b71ee686d93c78f8cf0352e4
Vincent Torri <vincent.torri@gmail.com>
2019-07-22 16:16:47 +0200

    libbs2b: fix dll creation

--- packages/libbs2b/install.sh
+++ packages/libbs2b/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-subunit > ../config.log 2>&1
+sed -i -e 's/libbs2b_la_LDFLAGS =/libbs2b_la_LDFLAGS = -no-undefined/g' src/Makefile.in
+
+./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
