08a27670cad7253bb1a1cd09cc840f116b00e2c9
Vincent Torri <vincent.torri@gmail.com>
2019-07-24 13:35:59 +0200

    dbus: fix dbus-1.pc and speed up a bit configure

--- packages/dbus/install.sh
+++ packages/dbus/install.sh
@@ -4,6 +4,6 @@
 
 sed -i -e 's/-m 700//g' test/Makefile.in
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-embedded-tests --disable-modular-tests --disable-tests --with-dbus-session-bus-default-address=autolaunch: > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --disable-embedded-tests --disable-modular-tests --disable-tests --disable-relocation --disable-selinux --disable-apparmor --disable-libaudit --disable-inotify --disable-kqueue --disable-launchd --disable-systemd --disable-epoll --disable-x11-autolaunch --with-dbus-session-bus-default-address=autolaunch: > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
