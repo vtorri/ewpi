069f77f53f80cff1f2254ae272398ddfc797f1fc
Vincent Torri <vincent.torri@gmail.com>
2019-09-26 16:01:45 +0200

    harfbuzz: update to 2.6.1, needs a fix to compile

--- packages/harfbuzz/install.sh
+++ packages/harfbuzz/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
+sed -i -e "s/{ S }/{ S; }/g" src/hb-icu.cc
+
 ./configure --prefix=$3 --host=$4 --disable-static --with-glib=no --disable-gtk-doc-html --disable-gtk-doc-pdf --with-cairo=no --with-fontconfig=no --with-freetype=yes --with-graphite2=yes --with-icu=yes > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
