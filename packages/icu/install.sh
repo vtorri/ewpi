ab0bf9c950756d273ed584a347d940bacdf7df6c
Vincent Torri <vincent.torri@gmail.com>
2019-09-26 16:02:29 +0200

    ICU: update to 65.1RC

--- packages/icu/install.sh
+++ packages/icu/install.sh
@@ -20,15 +20,16 @@ cd source
 
 ./runConfigureICU MinGW --prefix=$3 --host=$4 $cross --enable-tools --disable-tests --disable-samples > ../../config.log 2>&1
 
-make -j $jobopt install > ../../make.log 2>&1
+make -j $jobopt > ../../make.log 2>&1
+make install >> ../../make.log 2>&1
 
 mv $3/lib/*.dll $3/bin
 sed -i -e \
-    's/Libs: -licuin64/Libs: -L${libdir} -licuin/g' \
+    's/Libs: -licuin65/Libs: -L${libdir} -licuin/g' \
     $3/lib/pkgconfig/icu-i18n.pc
 sed -i -e \
-    's/Libs: -licuio64/Libs: -L${libdir} -licuio/g' \
+    's/Libs: -licuio65/Libs: -L${libdir} -licuio/g' \
     $3/lib/pkgconfig/icu-io.pc
 sed -i -e \
-    's/-licuuc64 -licudt64/-licuuc -licudt/g' \
+    's/-licuuc65 -licudt65/-licuuc -licudt/g' \
     $3/lib/pkgconfig/icu-uc.pc
