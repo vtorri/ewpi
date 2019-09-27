477de2539e7ee9d7058707242b0bba369d448d06
Vincent Torri <vincent.torri@gmail.com>
2019-09-27 14:37:24 +0200

    pixman: update to 0.38.4

--- packages/pixman/install.sh
+++ packages/pixman/install.sh
@@ -2,6 +2,25 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-gtk --disable-libpng > ../config.log 2>&1
+cp ../cross_toolchain.txt .
 
-make -j $jobopt $verbmake install > ../make.log 2>&1
+if test "x$4" = "xi686-w64-mingw32" ; then
+    sed -i -e 's/@cpu_family@/x86/g;s/@cpu@/i686/g' cross_toolchain.txt
+else
+    sed -i -e 's/@cpu_family@/x86_64/g;s/@cpu@/x86_64/g' cross_toolchain.txt
+fi
+
+sed -i -e "s/@host@/$4/g;s/@arch@/$1/g;s|@prefix@|$3|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+meson .. \
+      --prefix=$3 \
+      --libdir=lib \
+      --buildtype=release \
+      --strip \
+      --cross-file ../cross_toolchain.txt \
+      --default-library shared \
+      -Dgtk=disabled \
+      -Dlibpng=disabled > ../../config.log 2>&1
+
+ninja $verbninja install > ../../make.log 2>&1
