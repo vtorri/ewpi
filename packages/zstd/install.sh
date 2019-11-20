ef83f61e01e6bf337923b188ac2fffefdc6c12bf
Vincent Torri <vincent.torri@gmail.com>
2019-11-20 08:47:28 +0100

    zstd: fix AR value

--- packages/zstd/install.sh
+++ packages/zstd/install.sh
@@ -2,7 +2,7 @@
 
 . ../../common.sh
 
-make -j $jobopt CC=$4-gcc WINDRES=$4-windres > ../make.log 2>&1
+make -j $jobopt CC=$4-gcc WINDRES=$4-windres AR=$4-ar > ../make.log 2>&1
 cp lib/dll/libzstd.dll $3/bin
 cp lib/dll/libzstd.lib $3/lib/libzstd.dll.a
 cp lib/zstd.h lib/common/zstd_errors.h lib/deprecated/zbuff.h lib/dictBuilder/zdict.h $3/include
