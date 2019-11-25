589a17720228a6e6140afd4fbbcf4e357c69b34c
vtorri <vtorri@localhost.localdomain>
2019-11-25 21:00:39 +0100

    pixmap: remove tests

--- packages/pixman/install.sh
+++ packages/pixman/install.sh
@@ -12,6 +12,8 @@ fi
 
 sed -i -e "s/@host@/$4/g;s/@arch@/$1/g;s|@prefix@|$3|g" cross_toolchain.txt
 
+sed -i -e "s/subdir('test')//g" meson.build
+
 rm -rf builddir && mkdir builddir && cd builddir
 meson .. \
       --prefix=$3 \
