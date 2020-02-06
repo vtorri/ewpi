3b480c99a6c15f5998d93b7717a1946ff6d2c640
Vincent Torri <vincent.torri@gmail.com>
2020-02-06 06:15:59 +0100

    libbs2b: fir compilation on Windows

--- packages/libbs2b/install.sh
+++ packages/libbs2b/install.sh
@@ -3,7 +3,13 @@
 . ../../common.sh
 
 sed -i -e 's/libbs2b_la_LDFLAGS =/libbs2b_la_LDFLAGS = -no-undefined/g' src/Makefile.in
-sed -i -e 's/AC_FUNC_MALLOC//' configure.ac
+case ${EWPI_OS} in
+    MSYS*|MINGW*)
+    ;;
+    *)
+	sed -i -e 's/AC_FUNC_MALLOC//' configure.ac
+    ;;
+esac
 
 ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
