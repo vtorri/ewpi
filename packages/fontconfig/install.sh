e00f766a6a250bb94e579188feb5d02f819c7afe
Vincent Torri <vincent.torri@gmail.com>
2020-04-03 16:55:58 +0200

    fontconfig: fix issue with grep and RM detection

--- packages/fontconfig/install.sh
+++ packages/fontconfig/install.sh
@@ -7,6 +7,9 @@ sed -i -e 's/po-conf test/po-conf/g' Makefile.in
 # for fontconfig DLL
 export PATH=${EWPI_PWD}/src/.libs:$PATH
 
+# detection of RM
+sed -i -e 's|_predefined_rm=.*$|_predefined_rm=|p' configure
+
 ./configure --prefix=$3 --host=$4 --disable-static --enable-iconv --with-libiconv=$3 --with-expat=$3 --disable-docs > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
