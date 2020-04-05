685968b3db3ef4823d440303de3bc72aca0c3507
Vincent Torri <vincent.torri@gmail.com>
2020-04-05 11:26:28 +0200

    try to fix yasm compilation when antivirus are too strict

--- packages/yasm/install.sh
+++ packages/yasm/install.sh
@@ -4,4 +4,6 @@
 
 ./configure --prefix=$3 --host=$4 > ../config.log 2>&1
 
+#sed -i -e 's|$(top_builddir)/genstring$(EXEEXT) license_msg $@ $(srcdir)/COPYING|echo "static const char *license_msg[] = { \"Yasm\", \"BSD\" };" > license.c|g' Makefile
+
 make -j $jobopt $verbmake install > ../make.log 2>&1
