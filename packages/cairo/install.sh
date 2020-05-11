cdcb25162d9ae75443932fdd23b0e82138ad0a2f
Vincent Torri <vincent.torri@gmail.com>
2020-05-11 21:57:00 +0200

    remove useless cairo package

--- packages/cairo/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-. ../../common.sh
-
-sed -i -e 's/-Wp,-D_FORTIFY_SOURCE=2//g' configure
-
-./configure --prefix=$3 --host=$4 --disable-static --disable-xlib --disable-quartz --disable-png --enable-ft --disable-gtk-doc-html --disable-ps --disable-pdf --disable-svg > ../config.log 2>&1
-
-make -j $jobopt $verbmake install > ../make.log 2>&1
