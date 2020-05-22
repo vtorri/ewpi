dd84fc0edcd5170ee51f431964fbab2f0fd40621
Vincent Torri <vincent.torri@gmail.com>
2020-05-22 09:01:54 +0200

    libjpeg: fix prefix in pkgconfig file

--- packages/libjpeg/install.sh
+++ packages/libjpeg/install.sh
@@ -14,26 +14,22 @@ fi
 
 sed -i -e "s|@host@|$4|g;s|@proc@|$proc|g" cross_toolchain.txt
 
-export CFLAGS="$machine -I.. $CFLAGS"
-export LDFLAGS="$machine $LDFLAGS"
+rm -rf builddir && mkdir builddir && cd builddir
 
 cmake \
-    -DCMAKE_TOOLCHAIN_FILE=cross_toolchain.txt \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
     -DCMAKE_INSTALL_PREFIX=$prefix_unix \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-I.. -O2 -pipe -march=$1" \
+    -DCMAKE_C_FLAGS="-O2 -pipe $machine -march=$1" \
     -DCMAKE_EXE_LINKER_FLAGS="-s" \
-    -DCMAKE_SHARED_LINKER_FLAGS="-s" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-s $machine" \
     -DENABLE_STATIC=FALSE \
     -DREQUIRE_SIMD=TRUE \
     -DWITH_JAVA=FALSE \
     -DWITH_JPEG8=TRUE \
     -DWITH_TURBOJPEG=FALSE \
     -G "Unix Makefiles" \
-    . >> ../config.log 2>&1
+    .. >> ../../config.log 2>&1
 
-make -j $jobopt install > ../make.log 2>&1
-
-sed -i -e "s|$prefix_unix|$3|g" $3/lib/pkgconfig/libjpeg.pc
-sed -i -e "s|$prefix_unix|$3|g" $3/lib/pkgconfig/libturbojpeg.pc
+make -j $jobopt install > ../../make.log 2>&1
