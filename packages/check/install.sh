2e4927becbeeed1faf364e134e9631e734d1d43a
Vincent Torri <vincent.torri@gmail.com>
2020-06-23 13:59:24 +0200

    update check to 0.15.0

--- packages/check/install.sh
+++ packages/check/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
+export LIBS="-lregex $LIBS"
+
 ./configure --prefix=$3 --host=$4 --disable-static --disable-subunit > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
