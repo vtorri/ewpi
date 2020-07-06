898a156ba6b647cedc574684447460b4fd813755
Vincent Torri <vincent.torri@gmail.com>
2020-07-06 23:11:06 +0200

    add libvmaf, libaom and libavif

---
+++ packages/libaom/install.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+. ../../common.sh
+
+git fetch && git fetch --tags
+git checkout v2.0.0
+
+cp ../cross_toolchain.txt .
+
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@host@|$4|g;s|@proc@|$proc|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS="-O2 -pipe $machine -march=$1 -I$3/include" \
+    -DCMAKE_EXE_LINKER_FLAGS="-s" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-s $machine" \
+    -DBUILD_SHARED_LIBS=TRUE \
+    -DCONFIG_AV1_DECODER=1 \
+    -DCONFIG_AV1_ENCODER=1 \
+    -DCONFIG_MULTITHREAD=1 \
+    -DCONFIG_TUNE_VMAF=1 \
+    -DENABLE_DOCS=FALSE \
+    -DENABLE_EXAMPLES=FALSE \
+    -DENABLE_TESTS=FALSE \
+    -DENABLE_TOOLS=FALSE \
+    -G "Unix Makefiles" \
+    .. >> ../../config.log 2>&1
+
+make -j $jobopt install > ../../make.log 2>&1
---
+++ packages/libavif/install.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+. ../../common.sh
+
+cp ../cross_toolchain.txt .
+
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@host@|$4|g;s|@proc@|$proc|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_FLAGS="-O2 -pipe $machine -march=$1" \
+    -DCMAKE_EXE_LINKER_FLAGS="-s" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-s $machine" \
+    -DBUILD_SHARED_LIBS=TRUE \
+    -DAVIF_CODEC_AOM:BOOL=ON \
+    -DAVIF_CODEC_DAV1D:BOOL=ON \
+    -G "Unix Makefiles" \
+    .. >> ../../config.log 2>&1
+
+make -j $jobopt install > ../../make.log 2>&1
