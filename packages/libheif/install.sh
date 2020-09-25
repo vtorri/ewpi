5a731c3d37bc39f9b3aa786f838cc2e81cf294e5
Vincent Torri <vincent.torri@gmail.com>
2020-09-25 11:49:49 +0200

    update libheif to 1.9.1

--- packages/libheif/install.sh
+++ packages/libheif/install.sh
@@ -6,6 +6,6 @@ export CPPFLAGS="-D_WIN32_WINNT=0x0600 $CPPFLAGS"
 
 sed -i -e 's/aom_img_fmt_t img_format;/aom_img_fmt_t img_format = AOM_IMG_FMT_NONE;/g' libheif/heif_encoder_aom.cc
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-examples --disable-tests > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --disable-go --disable-examples --disable-tests > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
