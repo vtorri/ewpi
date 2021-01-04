8873d73cabc014b181e469289ba190a64e71cc8d
Vincent Torri <vincent.torri@gmail.com>
2021-01-04 15:52:30 +0100

    gettext, libpng: force libtool to build DLL

--- packages/gettext/install.sh
+++ packages/gettext/install.sh
@@ -4,6 +4,8 @@
 
 cd gettext-runtime
 
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
 ./configure --prefix=$3 --host=$4 --disable-static --disable-c++ --disable-java --disable-native-java --enable-threads=windows --disable-rpath --disable-libasprintf --disable-curses --disable-acl --with-libiconv-prefix=$3 > ../../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../../make.log 2>&1
