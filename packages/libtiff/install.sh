4f1f338cde1ff4586d1f73239cdf08fafa1ab8d4
Vincent Torri <vincent.torri@gmail.com>
2021-01-04 15:54:53 +0100

    libtiff : update to 4.2.0, fix creation of DLL, and circular dependency with libwebp

--- packages/libtiff/install.sh
+++ packages/libtiff/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --with-zlib-include-dir=$3/include --with-zlib-lib-dir=$3/lib --with-jpeg-include-dir=$3/include --with-jpeg-lib-dir=$3/lib --with-lzma-include-dir=$3/include --with-lzma-lib-dir=$3/lib --disable-cxx > ../config.log 2>&1
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
+./configure --prefix=$3 --host=$4 --disable-static --with-zlib-include-dir=$3/include --with-zlib-lib-dir=$3/lib --with-jpeg-include-dir=$3/include --with-jpeg-lib-dir=$3/lib --with-lzma-include-dir=$3/include --with-lzma-lib-dir=$3/lib --with-webp-include-dir=$3/include --with-webp-lib-dir=$3/lib --disable-cxx > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
