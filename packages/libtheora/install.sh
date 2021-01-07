2567070856db664188be12c11b497f2a7dabf91c
Vincent Torri <vincent.torri@gmail.com>
2021-01-07 16:37:41 +0100

    libtheora: force libtool to crate DLL

--- packages/libtheora/install.sh
+++ packages/libtheora/install.sh
@@ -13,6 +13,8 @@ case ${EWPI_OS} in
     ;;
 esac
 
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
 ./configure --prefix=$3 --host=$4 --disable-static --disable-spec --disable-examples $disableogg $disablevorbis > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
