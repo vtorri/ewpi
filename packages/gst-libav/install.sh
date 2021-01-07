87bfd702397c7c3df22ea26f5e0894e2f5444825
Vincent Torri <vincent.torri@gmail.com>
2021-01-07 16:46:51 +0100

    gst-libav: formatting of install.sh

--- packages/gst-libav/install.sh
+++ packages/gst-libav/install.sh
@@ -19,6 +19,7 @@ meson .. \
       --buildtype=release \
       --strip \
       --cross-file ../cross_toolchain.txt \
-      --default-library shared > ../../config.log 2>&1
+      --default-library shared \
+      > ../../config.log 2>&1
 
 ninja $verbninja install > ../../make.log 2>&1
