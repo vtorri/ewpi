45d284b4442bf1a5a7d16306aa79ddb994c88921
Vincent Torri <vincent.torri@gmail.com>
2021-01-07 16:49:57 +0100

    libxml2: force libtool to crate DLL

--- packages/libxml2/install.sh
+++ packages/libxml2/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
 ./configure --prefix=$3 --host=$4 --disable-static --with-python=no --with-icu=yes > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
