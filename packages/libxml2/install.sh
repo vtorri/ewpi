84ebe6a023f8e3220b877be8483f64f1c4515fe1
Vincent Torri <vincent.torri@gmail.com>
2021-01-08 07:17:52 +0100

    libxml2: since ICU 68, TRUE is not defined anymore, use 1 instead

--- packages/libxml2/install.sh
+++ packages/libxml2/install.sh
@@ -4,6 +4,8 @@
 
 sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
 
+sed -i -e "s/TRUE/1/g" encoding.c
+
 ./configure --prefix=$3 --host=$4 --disable-static --with-python=no --with-icu=yes > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
