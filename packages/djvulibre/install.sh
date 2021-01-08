11e94e460f40668c3190311669e756db2a165556
Vincent Torri <vincent.torri@gmail.com>
2021-01-08 09:47:09 +0100

    djvulibre: add iconv support

--- packages/djvulibre/install.sh
+++ packages/djvulibre/install.sh
@@ -4,6 +4,8 @@
 
 sed -i -e 's/libdjvulibre_la_CPPFLAGS =/libdjvulibre_la_CPPFLAGS = -DDJVUAPI_EXPORT -DDDJVUAPI_EXPORT -DMINILISPAPI_EXPORT/g' libdjvu/Makefile.in
 
+export LIBS="-liconv $LIBS"
+
 ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
