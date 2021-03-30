d8d998041900f00e5e3b8ed81e12977531648b5c
Vincent Torri <vincent.torri@gmail.com>
2021-03-30 14:12:55 +0200

    Remove unused libexif package

--- packages/libexif/install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-. ../../common.sh
-
-./configure --prefix=$3 --host=$4 --disable-static --disable-internal-docs --disable-ship-binaries > ../config.log 2>&1
-
-make -j $jobopt $verbmake install > ../make.log 2>&1
