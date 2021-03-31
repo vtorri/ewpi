ede97d0e0cf021c833ab68dd82529589a8709ef5
Vincent Torri <vincent.torri@gmail.com>
2021-03-31 09:13:11 +0200

    libarchive: force libtool to create DLL

--- packages/libarchive/install.sh
+++ packages/libarchive/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
 ./configure --prefix=$3 --host=$4 --disable-static --enable-bsdtar=shared --enable-bsdcat=shared --enable-bsdcpio=shared --without-xml2 --with-expat --without-nettle > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
