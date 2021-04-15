37b3c738cecf977ca57accc697b13d2b7bcffb1f
Vincent Torri <vincent.torri@gmail.com>
2021-04-15 06:54:41 +0200

    and add libexif files....

---
+++ packages/libexif/install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+. ../../common.sh
+
+sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
+
+./configure --prefix=$3 --host=$4 --disable-static --disable-internal-docs --disable-ship-binaries > ../config.log 2>&1
+
+make -j $jobopt $verbmake install > ../make.log 2>&1
