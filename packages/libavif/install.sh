c17149e71cf6d665b4ccd09173009d53a2e2a8b3
Vincent Torri <vincent.torri@gmail.com>
2021-05-21 15:44:47 +0200

    libavif : fix compilation

--- packages/libavif/install.sh
+++ packages/libavif/install.sh
@@ -17,9 +17,15 @@ sed -i -e "s|@host@|$4|g;s|@proc@|$proc|g;s|@prefix@|$prefix_unix|g" cross_toolc
 
 sed -i -e "s|aom REQUIRED|aom|g" CMakeLists.txt
 
-sed -i -e "s|%zu|%Iu|g" apps/avifenc.c
-sed -i -e "s|%zu|%Iu|g" apps/avifdec.c
-sed -i -e "s|%zu|%Iu|g" apps/shared/avifutil.c
+case ${EWPI_OS} in
+    MSYS*|MINGW*)
+        sed -i -e "s|%zu|%Iu|g" apps/avifenc.c
+        sed -i -e "s|%zu|%Iu|g" apps/avifdec.c
+        sed -i -e "s|%zu|%Iu|g" apps/shared/avifutil.c
+    ;;
+    *)
+    ;;
+esac
 
 rm -rf builddir && mkdir builddir && cd builddir
 
@@ -28,14 +34,22 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$prefix_unix \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-O2 -pipe $machine -march=$1 -I$3/include/libvmaf -D__USE_MINGW_ANSI_STDIO=0" \
+    -DCMAKE_C_FLAGS="-O2 -pipe $machine -march=$1 -I$3/include -I$3/include/libvmaf -D__USE_MINGW_ANSI_STDIO=0" \
     -DCMAKE_EXE_LINKER_FLAGS="-s" \
     -DCMAKE_SHARED_LINKER_FLAGS="-s $machine -L$3/lib" \
     -DBUILD_SHARED_LIBS=TRUE \
     -DAVIF_CODEC_AOM:BOOL=ON \
     -DAVIF_CODEC_DAV1D:BOOL=ON \
     -DAVIF_BUILD_APPS:BOOL=ON \
-    -G "Unix Makefiles" \
+    -G Ninja \
     .. > ../../config.log 2>&1
 
-make -j $jobopt install > ../../make.log 2>&1
+case ${EWPI_OS} in
+    MSYS*|MINGW*)
+    ;;
+    *)
+        sed -i -e "s|-I/usr/include||g" build.ninja
+    ;;
+esac
+
+ninja -j $jobopt install > ../../make.log 2>&1
