f19df87e2f362d06b2db170b15797812d6c52798
Vincent Torri <vincent.torri@gmail.com>
2021-05-25 10:09:55 +0200

    ffmpeg: remove libwavpack dependency

--- packages/ffmpeg/install.sh
+++ packages/ffmpeg/install.sh
@@ -3,8 +3,8 @@
 . ../../common.sh
 
 if test "x$4" = "xx86_64-w64-mingw32" ; then
-    targetOS="mingw64"
-    arch="x86_64"
+    targetOS=mingw64
+    arch=x86_64
 else
     targetOS="mingw32"
     arch="x86"
@@ -13,9 +13,9 @@ fi
 rm -rf builddir && mkdir builddir && cd builddir
 
 ../configure --prefix=$3 --disable-static --enable-shared \
+	     --pkg-config=pkg-config \
             --enable-cross-compile --cross-prefix=$4- \
 	    --sysroot=$3 --sysinclude=$3/include \
-            --host-os=$targetOS \
             --target-os=$targetOS --arch=$arch \
             --enable-gcrypt \
             --enable-libaom \
@@ -44,7 +44,6 @@ rm -rf builddir && mkdir builddir && cd builddir
             --enable-libtheora \
             --enable-libtls \
             --enable-libvorbis \
-            --enable-libwavpack \
             --enable-libwebp \
             --enable-libxml2 \
             > ../../config.log 2>&1
