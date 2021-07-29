b7f6146e4bb7352b32536c659f707d229fa99b20
Vincent Torri <vincent.torri@gmail.com>
2021-07-29 11:23:26 +0200

    Add NSIS installer

--- ewpi_strip.sh
+++ ewpi_strip.sh
@@ -5,3 +5,9 @@
 
 find $1/bin -name "*.dll" -exec $2-strip {} \; > strip.log 2>&1
 find $1/lib -name "*.dll" -exec $2-strip {} \; >> strip.log 2>&1
+
+cp -f ewpi.nsi.in ewpi.nsi
+path=`echo $1 | sed 's,/,\\\\\\\\,g'`
+echo $path
+
+sed -i -e "s|@prefix@|${path}|g" ewpi.nsi
