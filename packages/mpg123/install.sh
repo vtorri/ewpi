d804cf62abab172a195cf03c54c6fb5e9062becb
Vincent Torri <vincent.torri@gmail.com>
2021-08-06 09:49:29 +0200

    remove yasm support (seems to be deprecated)

--- packages/mpg123/install.sh
+++ packages/mpg123/install.sh
@@ -10,6 +10,6 @@ fi
 
 sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
 
-./configure --prefix=$3 --host=$4 --disable-static --with-cpu=$cpu --enable-yasm --enable-int-quality --with-default-audio=win32_wasapi > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --with-cpu=$cpu --enable-int-quality --with-default-audio=win32_wasapi > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
