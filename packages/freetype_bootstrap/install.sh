689f47af34c4f38dfea9c97104e9435055986bf4
Vincent Torri <vincent.torri@gmail.com>
2022-02-05 14:40:29 +0100

    use meson to build freetype

--- packages/freetype_bootstrap/install.sh
+++ packages/freetype_bootstrap/install.sh
@@ -2,8 +2,24 @@
 
 . ../../common.sh
 
-sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" builds/unix/configure
+cp ../cross_toolchain.txt .
 
-./configure --prefix=$3 --host=$4 --disable-static --with-harfbuzz=no --with-bzip2=no > ../config.log 2>&1
+if test "x$4" = "xi686-w64-mingw32" ; then
+    sed -i -e 's/@cpu_family@/x86/g;s/@cpu@/i686/g' cross_toolchain.txt
+else
+    sed -i -e 's/@cpu_family@/x86_64/g;s/@cpu@/x86_64/g' cross_toolchain.txt
+fi
 
-make -j $jobopt $verbmake install > ../make.log 2>&1
+sed -i -e "s/@host@/$4/g;s/@arch@/$1/g;s|@prefix@|$3|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+meson .. \
+      --prefix=$3 \
+      --libdir=lib \
+      --buildtype=release \
+      --strip \
+      --cross-file ../cross_toolchain.txt \
+      --default-library shared \
+      -Dharfbuzz=disabled > ../../config.log 2>&1
+
+ninja $verbninja install > ../../make.log 2>&1
