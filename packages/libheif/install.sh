a734de9e65a52266282f4ffc0dde8eadf9b8cad0
Vincent Torri <vincent.torri@gmail.com>
2022-05-16 21:42:23 +0200

    libheif: disable gdk-pixbuf plugin build

--- packages/libheif/install.sh
+++ packages/libheif/install.sh
@@ -4,6 +4,6 @@
 
 sed -i -e 's/aom_img_fmt_t img_format;/aom_img_fmt_t img_format = AOM_IMG_FMT_NONE;/g' libheif/heif_encoder_aom.cc
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-go --disable-examples --disable-tests > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --disable-go --disable-examples --disable-gdk-pixbuf --disable-tests > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
