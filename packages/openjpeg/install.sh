deb7d101f631ae1e0c429ff5e11d8a63ee03d619
Vincent Torri <vtorri@outlook.fr>
2022-09-13 06:35:36 +0200

    openjpeg: fix root dir when croscompiling

--- packages/openjpeg/install.sh
+++ packages/openjpeg/install.sh
@@ -4,8 +4,6 @@
 
 cp ../cross_toolchain.txt .
 
-sed -i '/typedef SSIZE_T ssize_t;/ d' src/lib/openjpip/sock_manager.c
-
 if test "x$4" = "xx86_64-w64-mingw32" ; then
     proc="AMD64"
     machine=-m64
@@ -16,15 +14,6 @@ fi
 
 sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
 
-case ${EWPI_OS} in
-    MSYS*|MINGW*)
-	codec="ON"
-    ;;
-    *)
-	codec="OFF"
-    ;;
-esac
-
 rm -rf builddir && mkdir builddir && cd builddir
 
 cmake \
@@ -33,9 +22,9 @@ cmake \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_STATIC_LIBS=OFF \
-    -DBUILD_CODEC:BOOL=$codec \
-    -DBUILD_JPIP:BOOL=OFF \
-    -DBUILD_PKGCONFIG_FILES:BOOL=ON \
+    -DBUILD_CODEC=OFF \
+    -DBUILD_JPIP=OFF \
+    -DBUILD_PKGCONFIG_FILES=ON \
     -G "Unix Makefiles" \
     .. > ../../config.log 2>&1
 
