4b697fc78d622554c9355d7a6472c9faa018d697
Vincent Torri <vtorri@outlook.fr>
2022-09-14 21:23:54 +0200

    libmysofa: fix root dir when croscompiling

--- packages/libmysofa/install.sh
+++ packages/libmysofa/install.sh
@@ -21,20 +21,9 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$prefix_unix \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_STATIC_LIBS:BOOL=OFF \
-    -DBUILD_TESTS:BOOL=OFF \
+    -DBUILD_STATIC_LIBS=OFF \
+    -DBUILD_TESTS=OFF \
     -G "Unix Makefiles" \
     .. > ../../config.log 2>&1
 
-case ${EWPI_OS} in
-    MSYS*|MINGW*)
-    ;;
-    *)
-        sed -i -e "s|-I/usr/include|-I$3/include|g" src/CMakeFiles/mysofa-shared.dir/includes_C.rsp
-        sed -i -e "s|/usr/lib/libz.a|-lz|g" src/CMakeFiles/mysofa-shared.dir/linklibs.rsp
-    ;;
-esac
-
 make -j $jobopt install > ../../make.log 2>&1
-
-cp src/libmysofa.dll.a $3/lib
