e46a486c305602ac09654a35ae7fc7b36b84ca9f
Vincent Torri <vtorri@outlook.fr>
2022-09-14 06:51:21 +0200

    add libyuv package (dep of libavif)

---
+++ packages/libyuv/install.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+. ../../common.sh
+
+cp ../cross_toolchain.txt .
+
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+
+sed -i -e "s|target_link_libraries( yuvconvert \${JPEG_LIBRARY} )|target_link_libraries( \${ly_lib_shared} \${JPEG_LIBRARY} )|g;s|PROGRAMS \${CMAKE_BINARY_DIR}/yuvconvert|TARGETS yuvconvert|g" CMakeLists.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -G "Unix Makefiles" \
+    .. > ../../config.log 2>&1
+
+make -j $jobopt install > ../../make.log 2>&1
