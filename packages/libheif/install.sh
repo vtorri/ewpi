77778bdb917d89f92aab1f27a692e8eb89726795
Vincent Torri <vtorri@outlook.fr>
2023-01-28 01:19:22 +0100

    update libde265 to 1.0.10 and libheif to 1.14.2

--- packages/libheif/install.sh
+++ packages/libheif/install.sh
@@ -2,8 +2,6 @@
 
 . ../../common.sh
 
-sed -i -e 's/aom_img_fmt_t img_format;/aom_img_fmt_t img_format = AOM_IMG_FMT_NONE;/g' libheif/heif_encoder_aom.cc
-
 ./configure --prefix=$3 --host=$4 --disable-static --disable-go --disable-examples --disable-gdk-pixbuf --disable-tests > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
