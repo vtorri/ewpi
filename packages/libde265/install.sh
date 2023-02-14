80a7f217d303ef2a9406884dc14cf86033dd7224
Vincent Torri <vtorri@outlook.fr>
2023-02-14 08:48:24 +0100

    libde265: remove tools compilation

--- packages/libde265/install.sh
+++ packages/libde265/install.sh
@@ -4,4 +4,6 @@
 
 ./configure --prefix=$3 --host=$4 --disable-static --disable-sherlock265 --disable-dec265 --enable-log-error --enable-encoder --enable-sse > ../config.log 2>&1
 
+sed -i -e "s/am__append_2) tools/am__append_2) /g" Makefile
+
 make -j $jobopt $verbmake install > ../make.log 2>&1
