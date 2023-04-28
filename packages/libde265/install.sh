8cceb1d4bcff79a87d40a7855b737c672466e037
Vincent Torri <vtorri@outlook.fr>
2023-04-28 08:55:04 +0200

    libde265: use cmake to speed up compilation

--- packages/libde265/install.sh
+++ packages/libde265/install.sh
@@ -2,8 +2,27 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-sherlock265 --disable-dec265 --enable-log-error --enable-encoder --enable-sse > ../config.log 2>&1
+cp ../cross_toolchain.txt .
 
-sed -i -e "s/am__append_2) tools/am__append_2) /g" Makefile
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
 
-make -j $jobopt $verbmake install > ../make.log 2>&1
+sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_SDL=OFF \
+    -G "Unix Makefiles" \
+    .. > ../../config.log 2>&1
+
+make -j $jobopt install > ../../make.log 2>&1
