be95ff2f5f2dadf440031f028ccc5b001f8fb1de
Vincent Torri <vtorri@outlook.fr>
2023-05-01 11:08:01 +0200

    add install files for the EFL

---
+++ packages/efl/install.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+. ../../common.sh
+
+cp ../cross_toolchain.txt .
+
+if test "x$4" = "xi686-w64-mingw32" ; then
+    sed -i -e 's/@cpu_family@/x86/g;s/@cpu@/i686/g' cross_toolchain.txt
+else
+    sed -i -e 's/@cpu_family@/x86_64/g;s/@cpu@/x86_64/g' cross_toolchain.txt
+fi
+
+sed -i -e "s/@host@/$4/g;s/@arch@/$1/g;s|@prefix@|$3|g" cross_toolchain.txt
+
+export EWPI_PREFIX=$HOME/ewpi_64
+export PATH=$EWPI_PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$EWPI_PREFIX/lib/pkgconfig
+export CPPFLAGS="-I$EWPI_PREFIX/include -DECORE_WIN32_WIP_POZEFLKSD"
+export CFLAGS="-O2 -pipe -pipe"
+export CXXFLAGS="-O2 -pipe"
+export LDFLAGS=-L$EWPI_PREFIX/lib
+
+rm -rf builddir
+meson setup \
+      --prefix=$HOME/efl_64 \
+      --libdir=lib \
+      --buildtype=release \
+      --strip \
+      --default-library shared \
+      -Dsystemd=false \
+      -Dpulseaudio=false \
+      -Dv4l2=false \
+      -Dlibmount=false \
+      -Deeze=false \
+      -Dx11=false \
+      -Dxinput2=false \
+      -Devas-loaders-disabler='pdf','ps','rsvg','json' \
+      -Dopengl=none \
+      -Dpixman=true \
+      -Dembedded-lz4=false \
+      -Dfribidi=true \
+      -Dinput=false \
+      -Dbuild-examples=false \
+      -Dbuild-tests=false \
+      -Dbindings='lua','cxx' \
+      -Dlua-interpreter=luajit \
+      -Delua=true \
+      builddir \
+      > ../config.log 2>&1
+
+ninja $verbninja -C builddir install > ../make.log 2>&1
