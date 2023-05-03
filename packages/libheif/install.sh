bd23f4fa70f44c97c69328c447e8e3789e37269f
Vincent Torri <vtorri@outlook.fr>
2023-05-03 14:10:34 +0200

    update libheif to 1.16.1

--- packages/libheif/install.sh
+++ packages/libheif/install.sh
@@ -2,6 +2,35 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-go --disable-examples --disable-gdk-pixbuf --disable-tests > ../config.log 2>&1
+cp ../cross_toolchain.txt .
 
-make -j $jobopt $verbmake install > ../make.log 2>&1
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_PLUGIN_LOADING=OFF \
+    -DWITH_EXAMPLES=OFF \
+    -DWITH_GDK_PIXBUF=OFF \
+    -DWITH_AOM_DECODER=OFF \
+    -DWITH_AOM_ENCODER=OFF \
+    -DWITH_DAV1D=OFF \
+    -DWITH_RAV1E=OFF \
+    -DWITH_SvtEnc=OFF \
+    -DWITH_X265=OFF \
+    -G "Unix Makefiles" \
+    .. > ../../config.log 2>&1
+
+make -j $jobopt install > ../../make.log 2>&1
