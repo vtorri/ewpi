fbbe9ee79a4f81b2a74d9d4221cbc3f779e6c8d7
Vincent Torri <vtorri@outlook.fr>
2023-06-18 20:26:02 +0200

    libgpg-error: install gpg-error-config script (until meson build system lands upstream)

--- packages/libgpg-error/install.sh
+++ packages/libgpg-error/install.sh
@@ -2,6 +2,6 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static --disable-doc --disable-tests > ../config.log 2>&1
+./configure --prefix=$3 --host=$4 --disable-static --enable-install-gpg-error-config --disable-doc --disable-tests > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
