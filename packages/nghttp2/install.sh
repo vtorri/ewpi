3433925f1cf5d9fdc823e0a64dd21695171db687
Vincent Torri <vtorri@outlook.fr>
2023-10-13 05:04:53 +0200

    update nghttp2 to 1.57.0

--- packages/nghttp2/install.sh
+++ packages/nghttp2/install.sh
@@ -4,4 +4,6 @@
 
 ./configure --prefix=$3 --host=$4 --disable-static --enable-lib-only > ../config.log 2>&1
 
+sed -i -e "s/HAVE_CLOCK_GETTIME/HAVE_CLOCK_GETTIME1/g" lib/nghttp2_time.c
+
 make -j $jobopt $verbmake install > ../make.log 2>&1
