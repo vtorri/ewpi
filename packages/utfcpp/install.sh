2098aa41c9d4435568b462896f8157b8bd62fbf0
Vincent Torri <vtorri@outlook.fr>
2024-01-30 23:23:40 +0100

    install utf8cpp with cmake

--- packages/utfcpp/install.sh
+++ packages/utfcpp/install.sh
@@ -2,8 +2,27 @@
 
 . ../../common.sh
 
-cd source
+cp ../cross_toolchain.txt .
 
-mkdir -p $3/include/utf8cpp/utf8
-cp utf8.h $3/include/utf8cpp
-cp utf8/* $3/include/utf8cpp/utf8
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -G "Unix Makefiles" \
+    .. > ../../config.log 2>&1
+
+
+make -j $jobopt install > ../../make.log 2>&1
