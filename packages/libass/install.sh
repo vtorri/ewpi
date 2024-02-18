b61df69e7c2a33660eaba22785bc2a7965fd8bf5
Vincent Torri <vtorri@outlook.fr>
2024-02-18 11:38:10 +0100

    libass: make libass compile with fedora

--- packages/libass/install.sh
+++ packages/libass/install.sh
@@ -4,6 +4,6 @@
 
 sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
 
-./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
+CPPFLAGS="$CPPFLAGS -I$HOME/ewpi_64/include -I$HOME/ewpi_64/include/fribidi -I$HOME/ewpi_64/include/freetype2 -I$HOME/ewpi_64/include/harfbuzz" ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
