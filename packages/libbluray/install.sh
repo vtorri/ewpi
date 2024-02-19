8f65edc9e8166aae9eac7a640c4dc0fff0e83ea4
Vincent Torri <vtorri@outlook.fr>
2024-02-19 09:37:39 +0100

    libblueray: fix build on fedora

--- packages/libbluray/install.sh
+++ packages/libbluray/install.sh
@@ -4,6 +4,6 @@
 
 sed -i -e "s/lt_cv_deplibs_check_method='file_magic ^x86 archive import|^x86 DLL'/lt_cv_deplibs_check_method=pass_all/g" configure
 
-CFLAGS="$CFLAGS -I$3/include/libxml2" ./configure --prefix=$3 --host=$4 --disable-static --disable-bdjava-jar > ../config.log 2>&1
+CFLAGS="$CFLAGS -I$3/include/libxml2 -I$3/include/freetype2" ./configure --prefix=$3 --host=$4 --disable-static --disable-bdjava-jar > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
