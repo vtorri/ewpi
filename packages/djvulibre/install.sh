d1827d3cb9f6902261f49664bd3a6fed07e347fc
Vincent Torri <vtorri@outlook.fr>
2024-03-01 14:20:55 +0100

    djvulibre: build only lib and xdg file

--- packages/djvulibre/install.sh
+++ packages/djvulibre/install.sh
@@ -4,6 +4,8 @@
 
 sed -i -e 's/libdjvulibre_la_CPPFLAGS =/libdjvulibre_la_CPPFLAGS = -DDJVUAPI_EXPORT -DDDJVUAPI_EXPORT -DMINILISPAPI_EXPORT/g' libdjvu/Makefile.in
 
+sed -i -e 's/SUBDIRS = libdjvu tools share \$(am__append_1) \$(am__append_2)/SUBDIRS = libdjvu desktopfiles/g' Makefile.in
+
 export LIBS="-liconv $LIBS"
 
 ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
