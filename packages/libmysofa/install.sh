5b90cd5460919d94d7617cabd76bc63bfdb917cd
Vincent Torri <vincent.torri@gmail.com>
2024-06-04 15:55:45 +0200

    libmysofa: install header files and fix configuration of ffmeg by renaming import lib

--- packages/libmysofa/install.sh
+++ packages/libmysofa/install.sh
@@ -2,6 +2,9 @@
 
 . ../../common.sh
 
+git fetch && git fetch --tags
+git checkout da9e4adc619ee3d1ae5e68da3ed14aa5e60b3ec1
+
 cp ../cross_toolchain.txt .
 
 if test "x$4" = "xx86_64-w64-mingw32" ; then
@@ -13,6 +16,7 @@ else
 fi
 
 sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+sed -i -e "s|PROPERTIES ARCHIVE_OUTPUT_NAME mysofa_shared)|PROPERTIES ARCHIVE_OUTPUT_NAME mysofa)|g" src/CMakeLists.txt
 
 rm -rf builddir && mkdir builddir && cd builddir
 
@@ -21,9 +25,11 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$prefix_unix \
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=$verbcmake \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
     -DBUILD_STATIC_LIBS=OFF \
     -DBUILD_TESTS=OFF \
-    -G "Unix Makefiles" \
+    -G "Ninja" \
     .. > ../../config.log 2>&1
 
-make -j $jobopt install > ../../make.log 2>&1
+cd ..
+ninja $verbninja -C builddir install > ../make.log 2>&1
