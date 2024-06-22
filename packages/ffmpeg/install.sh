e86bd124d9b63a0ed0164de2f4b47c8d8ca29acc
Vincent Torri <vtorri@outlook.fr>
2024-06-22 06:36:34 +0200

    ffmpeg: temporary fix

--- packages/ffmpeg/install.sh
+++ packages/ffmpeg/install.sh
@@ -13,7 +13,12 @@ fi
 sed -i -e 's/DEPWINDRES=$dep_cc//g;s/-D__USE_MINGW_ANSI_STDIO=1//g' configure
 sed -i -e 's/--preprocessor "$(DEPWINDRES) -E -xc-header -DRC_INVOKED $(CC_DEPFLAGS)"/$(foreach ARG,$(CC_DEPFLAGS),--preprocessor-arg "$(ARG)")/g' ffbuild/common.mak
 
+wget https://git.ffmpeg.org/gitweb/ffmpeg.git/blobdiff_plain/6caf34dbe0f0e406c49394c6c6552cc1345957b7..42982b5a5d461530a792e69b3e8abdd9d6d67052:/libavformat/rtmpdh.c
+patch -p1 < rtmpdh.c
+
 rm -rf builddir && mkdir builddir && cd builddir
+
+export CPPFLAGS="$CPPFLAGS -I$3/include/libxml2 -DWINICONV_CONST="
 
 ../configure --prefix=$3 --disable-static --enable-shared \
              --pkg-config=pkg-config \
