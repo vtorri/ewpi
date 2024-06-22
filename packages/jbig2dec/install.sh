587bd044cd158157e300580511c4c20f42eb61e3
Vincent Torri <vtorri@outlook.fr>
2024-06-22 11:02:17 +0200

    jbig2dec: remove compilation of the binary

--- packages/jbig2dec/install.sh
+++ packages/jbig2dec/install.sh
@@ -2,6 +2,8 @@
 
 . ../../common.sh
 
+sed -i -e "s/bin_PROGRAMS/#bin_PROGRAMS/g;s/jbig2dec_SOURCES/#jbig2dec_SOURCES/g;s/jbig2dec_LDADD/#jbig2dec_LDADD/g" Makefile.in
+
 ./configure --prefix=$3 --host=$4 --disable-static > ../config.log 2>&1
 
 make -j $jobopt $verbmake install > ../make.log 2>&1
