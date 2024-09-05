efb7bb39ee09aa001f56180d9c36d9e762ef477f
Vincent Torri <vtorri@outlook.fr>
2024-09-05 14:35:45 +0200

    shared-mime-info: remove the creation of the spec

--- packages/shared-mime-info/install.sh
+++ packages/shared-mime-info/install.sh
@@ -5,12 +5,18 @@
 # native compilation
 export CPPFLAGS="$CPPFLAGS -I$3/include/libxml2"
 
+rm -rf builddir
+
+sed -i -e "s/xmlto.found()/false/g" data/meson.build
+
 meson setup \
       --libdir=lib \
       --buildtype=release \
       --strip \
       --default-library shared \
       -Dupdate-mimedb=true \
+      -Dbuild-translations=false \
+      -Dbuild-tests=false \
       builddir > ../config.log 2>&1
 
 ninja $verbninja -C builddir > ../make.log 2>&1
