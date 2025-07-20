31715952c7a9ef0556738ae78f82dd12e6c901da
Vincent Torri <vtorri@outlook.fr>
2025-07-20 06:55:55 +0200

    update gst and al to 1.26.4 libogg to 1.3.6

--- packages/libogg/install.sh
+++ packages/libogg/install.sh
@@ -2,6 +2,29 @@
 
 . ../../common.sh
 
-./configure --prefix=$3 --host=$4 --disable-static >> ../config.log 2>&1
+cp ../cross_toolchain.txt .
 
-make -j $jobopt $verbmake install > ../make.log 2>&1
+if test "x$4" = "xx86_64-w64-mingw32" ; then
+    proc="AMD64"
+    machine=-m64
+else
+    proc="X86"
+    machine=-m32
+fi
+
+sed -i -e "s|@prefix@|$3|g;s|@host@|$4|g;s|@proc@|$proc|g;s|@winver@|$winver|g" cross_toolchain.txt
+
+rm -rf builddir && mkdir builddir && cd builddir
+
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=../cross_toolchain.txt \
+    -DCMAKE_INSTALL_PREFIX=$prefix_unix \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DINSTALL_DOCS=OFF \
+    -DINSTALL_PKG_CONFIG_MODULE=ON \
+    -DINSTALL_CMAKE_PACKAGE_MODULE=OFF \
+    -G "Ninja" \
+    .. > ../../config.log 2>&1
+
+ninja $verbninja -j $jobopt install > ../../make.log 2>&1
